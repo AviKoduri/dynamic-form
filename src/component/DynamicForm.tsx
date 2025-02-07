28ed58a7c1a3ff49a40cd66a1b8badab511278b4
praveenvuddagiri <vuddagiripraveen@gmail.com>
2025-02-07 13:20:58 +0530

    changes

--- src/component/DynamicForm.tsx
+++ src/component/DynamicForm.tsx
@@ -1,53 +1,74 @@
-import '../styles.css';
+import "../styles.css";
 import React, { useRef } from "react";
-import { Formik, Form, Field, ErrorMessage, FieldProps, FormikProps } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldProps,
+  FormikProps,
+} from "formik";
 import * as Yup from "yup";
-import _ from 'lodash';
-import { twMerge } from 'tailwind-merge';
+import _ from "lodash";
+import { twMerge } from "tailwind-merge";
+import Select from "react-select";
+
+type MultiSelectOption = { value: string; label: string };
 
 export interface FormField {
   id: number;
   label: string;
   name: string;
-  type: 'text' | 'email' | 'password' | 'textarea' | 'number' | 'enum' |"time" | "date" | "checkbox" | "radio" | "file"; 
+  type:
+    | "text"
+    | "email"
+    | "password"
+    | "textarea"
+    | "number"
+    | "enum"
+    | "time"
+    | "date"
+    | "checkbox"
+    | "radio"
+    | "file"
+    | "multiselect";
   required: boolean;
-  disabled?:boolean
+  disabled?: boolean;
   displayErrorMessage: boolean;
-  regexPattern?: string; 
+  regexPattern?: string;
   value?: number | string | boolean;
-  options?: { label: string; value: string }[];
+  options?: MultiSelectOption[];
   labelStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   inputStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   inputContainerStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   radioInputContainerStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
-
 }
 interface DynamicFormProps {
   formData: FormField[];
-  edit?:boolean;
+  edit?: boolean;
   containerStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   buttonStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   buttonContainerStyles?: {
-    className?:string;
-    style?:React.CSSProperties
+    className?: string;
+    style?: React.CSSProperties;
   };
   onSubmitFun?: (values: Record<string, any>) => void;
   formikRef?: React.RefObject<FormikProps<any>>;
@@ -65,66 +86,84 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
   const internalFormRef = useRef<FormikProps<any>>(null);
   const formikRef = externalFormRef || internalFormRef;
 
- const validationSchema = Yup.object().shape(
-  formData.reduce((schema, field) => {
-    let fieldValidation = Yup.string();
+  const validationSchema = Yup.object().shape(
+    formData.reduce((schema, field) => {
+      let fieldValidation: any = Yup.string();
 
-    switch (field.type) {
-      case 'email':
-        fieldValidation = Yup.string().email('Invalid email format');
-        break;
-      case 'number':
-        //@ts-ignore
-        fieldValidation = Yup.number().typeError(`${field.label} must be a number`);
-        break;
-      case 'password':
-        fieldValidation = Yup.string();
-        break;
-      case 'date':
-        //@ts-ignore
-        fieldValidation = Yup.date();
-        break;
-      case 'checkbox':
-        //@ts-ignore
-        fieldValidation = Yup.boolean().oneOf([true], `${field.label} is required`);
-        break;
-      case 'radio':
-        fieldValidation = Yup.string()
+      if (field.required) {
+        fieldValidation = fieldValidation.required(
+          `${field.label} is required`
+        );
+      }
+
+      if (field.regexPattern) {
+        fieldValidation = fieldValidation.matches(
+          new RegExp(field.regexPattern),
+          `${field.label} is invalid`
+        );
+      }
+
+      switch (field.type) {
+        case "email":
+          fieldValidation = fieldValidation.email("Invalid email format");
+          break;
+        case "number":
           //@ts-ignore
-          .oneOf(field?.options?.map((option) => option.value) || [], `Please select a valid ${field.label}`);
-        break;
-      default:
-        fieldValidation = Yup.string();
-    }
+          fieldValidation = Yup.number()
+            .typeError(`${field.label} must be a number`);
+          break;
+        case "password":
+          fieldValidation = Yup.string();
+          break;
+        case "multiselect":
+          fieldValidation = Yup.array()
+            .of(Yup.object().shape({
+              value: Yup.string().required("Value is required"),
+              label: Yup.string().required("Label is required"),
+            }));
+          break;
+        case "date":
+          //@ts-ignore
+          fieldValidation = Yup.date();
+          break;
+        case "checkbox":
+          //@ts-ignore
+          fieldValidation = Yup.boolean().oneOf(
+            [true],
+            `${field.label} is required`
+          );
+          break;
+        case "radio":
+          fieldValidation = Yup.string()
+            //@ts-ignore
+            .oneOf(
+              field?.options?.map((option) => option.value) || [],
+              `Please select a valid ${field.label}`
+            )
+            ;
+          break;
+      }
 
-    if (field.required) {
-      fieldValidation = fieldValidation.required(`${field.label} is required`);
-    }
+      schema[field.name] = fieldValidation;
+      return schema;
+    }, {} as Record<string, Yup.AnySchema>)
+  );
 
-    if (field.regexPattern) {
-      fieldValidation = fieldValidation.matches(
-        new RegExp(field.regexPattern),
-        `${field.label} is invalid`
-      );
-    }
-
-    schema[field.name] = fieldValidation;
-    return schema;
-  }, {} as Record<string, Yup.AnySchema>)
-);
-
-  
-  
   const initialValues = formData.reduce((values, field) => {
-    values[field.name] = edit && field.value !== undefined ? field.value : 
-      field.type === 'checkbox' ? false : 
-      field.type === 'number' ? '' : ''; 
+    values[field.name] =
+      edit && field.value !== undefined
+        ? field.value
+        : field.type === "checkbox"
+        ? false
+        : field.type === "number"
+        ? ""
+        : "";
     return values;
   }, {} as Record<string, string | number | boolean>);
 
   const renderField = (fields: FormField) => {
     console.log(fields);
-    
+
     switch (fields.type) {
       case "textarea":
         return (
@@ -133,9 +172,11 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
               <div>
                 <textarea
                   className={twMerge(
-                    'h-[45px] w-[100%] rounded-xl border-2',
-                    meta.touched && meta.error ? '!border-[red]' : 'border-grayColor',
-                    'pl-3',
+                    "h-[45px] w-[100%] rounded-xl border-2",
+                    meta.touched && meta.error
+                      ? "!border-[red]"
+                      : "border-grayColor",
+                    "pl-3",
                     fields?.inputStyles?.className
                   )}
                   style={fields?.inputStyles?.style}
@@ -143,14 +184,16 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                   placeholder={`Enter ${fields?.label}`}
                   {...field}
                 />
-                 {fields?.displayErrorMessage && meta.touched && meta.error && (
-                    <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">{meta.error}</div>
-                  )}
+                {fields?.displayErrorMessage && meta.touched && meta.error && (
+                  <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">
+                    {meta.error}
+                  </div>
+                )}
               </div>
             )}
           </Field>
         );
-  
+
       case "enum":
         return (
           <Field id={fields.name} name={fields.name} as="select">
@@ -158,16 +201,20 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
               <div>
                 <select
                   className={twMerge(
-                    'h-[45px] w-[100%] rounded-xl border-2',
-                    meta.touched && meta.error ? '!border-[red]' : 'border-grayColor',
-                    'pl-3',
+                    "h-[45px] w-[100%] rounded-xl border-2",
+                    meta.touched && meta.error
+                      ? "!border-[red]"
+                      : "border-grayColor",
+                    "pl-3",
                     fields?.inputStyles?.className
                   )}
                   style={fields?.inputStyles?.style}
                   disabled={fields?.disabled ?? false}
                   {...field}
                 >
-                  <option value="" disabled>Select {fields.label}</option>
+                  <option value="" disabled>
+                    Select {fields.label}
+                  </option>
                   {fields.options?.map((option) => (
                     <option key={option.value} value={option.value}>
                       {option.label}
@@ -175,42 +222,86 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                   ))}
                 </select>
                 {fields?.displayErrorMessage && meta.touched && meta.error && (
-                    <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">{meta.error}</div>
-                  )}
+                  <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">
+                    {meta.error}
+                  </div>
+                )}
               </div>
             )}
           </Field>
         );
 
-        case "radio":
-          return (
-            <Field id={fields.name} name={fields.name}>
-              {({ field, meta }: FieldProps) => (
-                <div                 className={twMerge(`flex flex-col gap-1 h-[85px]`, fields?.radioInputContainerStyles?.className)}
-                style={fields?.radioInputContainerStyles?.style}
->
-                  {fields.options?.map((option) => (
-                    <label key={option.value} className="inline-flex items-center mr-3">
-                      <input
-                        type="radio"
-                        className={twMerge(`h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-500`,  meta.touched && meta.error ? '!border-[red]' : 'border-grayColor',fields?.inputStyles?.className)}
-                        style={fields?.inputStyles?.style}
-                        name={fields.name}
-                        value={option.value}
-                        checked={field.value === option.value}
-                        disabled={fields?.disabled ? fields?.disabled : false}
-                        onChange={(e) => field.onChange(e)}
-                      />
-                      <span className="ml-2">{option.label}</span>
-                    </label>
-                  ))}
-                  {fields?.displayErrorMessage && meta.touched && meta.error && (
-                    <div className="text-[12px] leading-[13.92px] ml-2 text-[red]">{meta.error}</div>
+      case "multiselect":
+        return (
+          // <Field id={fields.name} name={fields.name} as="Select">
+          //   {({ field, form: { touched, errors }, meta }: FieldProps) => (
+              <div>
+                <Select
+                  options={fields.options}
+                  className={twMerge(
+                    fields?.inputStyles?.className
                   )}
-                </div>
-              )}
-            </Field>
-          );
+                  isDisabled={fields?.disabled ?? false}
+                  isMulti={true}
+                  name={fields.name}
+                  value={formikRef.current?.values[fields.name]}
+                  onChange={(val: any) => formikRef.current?.setFieldValue(fields.name, val)}
+                  placeholder={`Select ${fields.label}`}
+                />
+                {fields?.displayErrorMessage && formikRef.current?.touched && formikRef.current.errors[fields.name] && (
+                  <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">
+                   {formikRef.current.errors[fields.name]?.toString()}
+                  </div>
+                )}
+              </div>
+          //   )}
+          // </Field>
+        );
+
+      case "radio":
+        return (
+          <Field id={fields.name} name={fields.name}>
+            {({ field, meta }: FieldProps) => (
+              <div
+                className={twMerge(
+                  `flex flex-col gap-1 h-[85px]`,
+                  fields?.radioInputContainerStyles?.className
+                )}
+                style={fields?.radioInputContainerStyles?.style}
+              >
+                {fields.options?.map((option) => (
+                  <label
+                    key={option.value}
+                    className="inline-flex items-center mr-3"
+                  >
+                    <input
+                      type="radio"
+                      className={twMerge(
+                        `h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-500`,
+                        meta.touched && meta.error
+                          ? "!border-[red]"
+                          : "border-grayColor",
+                        fields?.inputStyles?.className
+                      )}
+                      style={fields?.inputStyles?.style}
+                      name={fields.name}
+                      value={option.value}
+                      checked={field.value === option.value}
+                      disabled={fields?.disabled ? fields?.disabled : false}
+                      onChange={(e) => field.onChange(e)}
+                    />
+                    <span className="ml-2">{option.label}</span>
+                  </label>
+                ))}
+                {fields?.displayErrorMessage && meta.touched && meta.error && (
+                  <div className="text-[12px] leading-[13.92px] ml-2 text-[red]">
+                    {meta.error}
+                  </div>
+                )}
+              </div>
+            )}
+          </Field>
+        );
       default:
         return (
           <Field id={fields.name} name={fields.name}>
@@ -218,10 +309,12 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
               <div>
                 <input
                   className={twMerge(
-                    'h-[45px] w-[100%] rounded-xl',
-                    fields?.type !== "file" && 'border-2',
-                    meta.touched && meta.error ? '!border-[red]' : 'border-gray-700',
-                    'pl-3',
+                    "h-[45px] w-[100%] rounded-xl",
+                    fields?.type !== "file" && "border-2",
+                    meta.touched && meta.error
+                      ? "!border-[red]"
+                      : "border-gray-700",
+                    "pl-3",
                     fields?.inputStyles?.className
                   )}
                   disabled={fields?.disabled ?? false}
@@ -230,9 +323,11 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                   placeholder={`Enter ${fields?.label}`}
                   {...field}
                 />
-                 {fields?.displayErrorMessage && meta.touched && meta.error && (
-                    <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">{meta.error}</div>
-                  )}
+                {fields?.displayErrorMessage && meta.touched && meta.error && (
+                  <div className="text-[12px] leading-[13.92px] ml-2 text-[red] mt-2">
+                    {meta.error}
+                  </div>
+                )}
               </div>
             )}
           </Field>
@@ -252,12 +347,15 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
     >
       {({ handleSubmit }) => (
         <Form onSubmit={handleSubmit} className="">
-          <div style={containerStyles?.style} className={containerStyles?.className}>
+          <div
+            style={containerStyles?.style}
+            className={containerStyles?.className}
+          >
             {formData.map((field) => (
               <div
                 key={field.id}
                 className={twMerge(
-                  'flex flex-col gap-1 h-[85px]',
+                  "flex flex-col gap-1 h-[85px]",
                   field?.inputContainerStyles?.className
                 )}
                 style={field?.inputContainerStyles?.style}
@@ -265,12 +363,13 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                 <label
                   htmlFor={field.name}
                   className={twMerge(
-                    'text-[16px] font-semibold',
+                    "text-[16px] font-semibold",
                     field?.labelStyles?.className
                   )}
                   style={field?.labelStyles?.style}
                 >
-                  {field.label}{" "}{field.required&&<span className='text-[red]'>*</span>}
+                  {field.label}{" "}
+                  {field.required && <span className="text-[red]">*</span>}
                 </label>
 
                 {renderField(field)}
@@ -284,11 +383,14 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
               </div>
             ))}
           </div>
-          <div style={buttonContainerStyles?.style} className={buttonContainerStyles?.className}>
+          <div
+            style={buttonContainerStyles?.style}
+            className={buttonContainerStyles?.className}
+          >
             <button
               type="submit"
               className={twMerge(
-                'px-5 py-2 bg-blue-500 text-white rounded-md text-center font-semibold mt-2',
+                "px-5 py-2 bg-blue-500 text-white rounded-md text-center font-semibold mt-2",
                 buttonStyles?.className
               )}
               style={buttonStyles?.style}
