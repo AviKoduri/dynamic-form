e311f19d0ea6b282a15bb2c20ed6328aa6c8bd9c
Avinash Koduri <saiavinashkoduri@gmail.com>
2025-02-07 11:26:56 +0530

    added start

--- src/component/DynamicForm.tsx
+++ src/component/DynamicForm.tsx
@@ -270,7 +270,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
                   )}
                   style={field?.labelStyles?.style}
                 >
-                  {field.label}{" "}<span className='text-[red]'>*</span>
+                  {field.label}{" "}{field.required&&<span className='text-[red]'>*</span>}
                 </label>
 
                 {renderField(field)}
